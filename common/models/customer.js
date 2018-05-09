265a4034d9c35a3a5b45939ca0a58603e2a07829
Sergey Kanygin <sskanygin@gmail.com>
2018-05-09 22:50:14 +0300

    Add spotify methods.

--- common/models/customer.js
+++ common/models/customer.js
@@ -10,12 +10,11 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 const accessToken = [
-  'BQBuscPdC7vRMlSlNck_dye9E-Kf-hnpxI162WYXsdl94lgvFcXjNt3Owy',
-  'C06Dtg_u8vVYJECLO2zSDn2Pnp5TyZIaZ_fledh3duTr-EgbF7GMus9OGC',
-  'j3cbnODn7dHc5IqrD34Y0SXqSzBST12-mSl5ue3hcLkTmPvt6xcpnKwH6D',
-  'XvRvdwNU6kHeclRO5DZ0CKUdglnF2SOBNlI_oyxba9CxoqwEzaXB7jmlnhxA',
+  'BQDGOm8iKGO7hCi-RRfcN0OHqEm5IFknZSDyWWp9-FK2BTj1sEdiAGJ174oAMFtjtOIXLPHlxMOSEpAIH6KkHoaxEAkYEjP2DS37uA5G5hbwBOheGOolC6il-9r9yI7yirn29FYF3VIhcukuIWRsZMatmefEkKA9zTKm5j3cC-bhc3s0f_Q35wKjsqKuWEaFnqPMPTffKcPfGUvoVUUuJ0oPtuDk88PptSXzRN8Ayg',
 ]
-  .join('')
+  .join('');
+
+const userId = '22ax75yxa77ecwcskbahjgsaa';
 
 spotifyApi.setAccessToken(accessToken);
 
@@ -24,7 +23,7 @@ module.exports = function (Customer) {
 
     spotifyApi
       .createPlaylist(
-        '22ax75yxa77ecwcskbahjgsaa',
+        userId,
         'My Cool Private Playlist',
         {'public': false}
       )
@@ -46,8 +45,7 @@ module.exports = function (Customer) {
   });
 
   // https://accounts.spotify.com/authorize?client_id=3972abbe8fb6496ca4f60724ac85be47&redirect_uri=http://localhost:4300&scope=user-read-private%20user-read-email%20playlist-modify-public%20playlist-modify-private&response_type=token&state=123
-  // http://localhost:4300/#access_token=BQBuscPdC7vRMlSlNck_dye9E-Kf-hnpxI162WYXsdl94lgvFcXjNt3OwyC06Dtg_u8vVYJECLO2zSDn2Pnp5TyZIaZ_fledh3duTr-EgbF7GMus9OGCj3cbnODn7dHc5IqrD34Y0SXqSzBST12-mSl5ue3hcLkTmPvt6xcpnKwH6DXvRvdwNU6kHeclRO5DZ0CKUdglnF2SOBNlI_oyxba9CxoqwEzaXB7jmlnhxA&token_type=Bearer&expires_in=3600&state=123
-
+  // http://localhost:4300/#access_token=BQDGOm8iKGO7hCi-RRfcN0OHqEm5IFknZSDyWWp9-FK2BTj1sEdiAGJ174oAMFtjtOIXLPHlxMOSEpAIH6KkHoaxEAkYEjP2DS37uA5G5hbwBOheGOolC6il-9r9yI7yirn29FYF3VIhcukuIWRsZMatmefEkKA9zTKm5j3cC-bhc3s0f_Q35wKjsqKuWEaFnqPMPTffKcPfGUvoVUUuJ0oPtuDk88PptSXzRN8Ayg&token_type=Bearer&expires_in=3600&state=123
 
   Customer.getRecommendations = (cb) => {
     const url = 'https://api.spotify.com/v1/recommendations';
@@ -78,6 +76,56 @@ module.exports = function (Customer) {
   };
 
   Customer.remoteMethod('getRecommendations', {
+    accepts: [],
+    http: {verb: 'get'},
+    returns: [
+      {arg: 'data', type: 'object', root: true},
+    ],
+  });
+
+  Customer.searchTrack = (cb) => {
+    const url = 'https://api.spotify.com/v1/recommendations';
+
+    spotifyApi.searchTracks('"Believer"')
+      .then(function (data) {
+        console.log('Search by "Love"', data.body);
+        cb(null, data.body);
+      }, function (err) {
+        console.error(err);
+        cb(err);
+      });
+  };
+
+  Customer.remoteMethod('searchTrack', {
+    accepts: [],
+    http: {verb: 'get'},
+    returns: [
+      {arg: 'data', type: 'object', root: true},
+    ],
+  });
+
+  Customer.addTracksToPlaylist = (cb) => {
+    const url = 'https://api.spotify.com/v1/recommendations';
+
+    // Add tracks to a playlist
+    spotifyApi.addTracksToPlaylist(
+      userId,
+      '5iRPpMyP7rSyeHB7F2CfwE',
+      [
+        'spotify:track:5VKSZDsidOFHrtlJb33Syo',
+        'spotify:track:3FvV3j98xtcylQF1drDc9j',
+      ]
+    )
+      .then(function (data) {
+        console.log('Added tracks to playlist!');
+        cb(null, data.body);
+      }, function (err) {
+        console.log('Something went wrong!', err);
+        cb(err);
+      });
+  };
+
+  Customer.remoteMethod('addTracksToPlaylist', {
     accepts: [],
     http: {verb: 'get'},
     returns: [
